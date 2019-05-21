aa8036e84d97813502f171ee92b8c2cfb27eb393
wangziyi0526 <568771088@qq.com>
2019-05-21 09:19:14 +0800

    -

--- test_one/string.py
+++ test_one/string.py
@@ -14,4 +14,51 @@ print(name)
 answer = 'doesn\'t like it !'
 print(answer)  # 利用 '\' 反斜杠来进行转义
 
+#part 2
+# \n 换行符  
+message = 'Kobe \nBryant'
+print(message)
+
+path = 'C:\name'
+print(path)
+print(r'C:\name')
+
+# 字符串拼接
+a = 'vue'
+print(a * 3 + '.js')
+
+# 根据字符串的索引去查找相应的值
+b = 'abcdefg'
+print(b[0]) # a
+print(b[1]) # b
+print(b[2]) # c
+print(b[3]) # d
+print(b[4]) # e
+print(b[5]) # f
+# 索引为负数的为倒序查找
+print(b[-1])# g
+# 切片
+print(b[2:4]) # cd
+print(b[0:3]) #abc
+print(b[:2])  #ab
+print(b[2:])  #cdefg
+print(b[:])   #abcdefg
+
+print(b[-2:]) #fg
+print(b[:-2]) #abcde
+
+print(b[2:] + b[:-2])  #cdefgabcde
+print(b[-3:] + b[:-3]) #efgabcd
+
+# length
+# 字符串的长度
+c = "abcdefghijklmn"
+print(len(c)) # 14 
+
+
+
+# 关于字符串这一块算是按照文档过了一遍 此时此刻我只想说一句python操作字符串太特么舒服了
+# 比JavaScript 骚气多了 哈哈哈当然JavaScript也很牛逼
+
+
 
